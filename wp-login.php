b36a83aefc67eabb5e3b9df1218bccef605e92eb
arth912 <arthrathod007@gmail.com>
2023-07-17 19:00:47 +0530

    first commit

---
+++ wp-login.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; Jack Parrish&#039;s Digital Resume and Portfolio &#8212; WordPress</title>
+	
+
+<style type="text/css">
+
+body.login {
+
+
+
+    }
+    
+body.login #login h1 a {
+
+
+
+
+
+}
+
+#loginform {
+
+
+
+
+
+}
+
+#login {
+
+}
+
+.login form .input, .login input[type="text"] {
+
+
+
+
+}
+
+.login label {
+
+}
+
+.wp-core-ui .button-primary {
+
+
+
+
+}
+
+.wp-core-ui .button-primary.focus, .wp-core-ui .button-primary.hover, .wp-core-ui .button-primary:focus, .wp-core-ui .button-primary:hover {
+
+
+}
+
+.login #backtoblog a, .login #nav a {
+
+}
+
+.login #backtoblog a:hover, .login #nav a:hover, .login h1 a:hover {
+
+}
+
+
+
+</style>
+
+<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link href="https://fonts.gstatic.com" crossorigin="anonymous" rel="preconnect">
+<link href="https://ajax.googleapis.com" rel="preconnect">
+<link href="https://fonts.googleapis.com" rel="preconnect">
+<link rel="stylesheet" id="dashicons-css" href="css/dashicons.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="css/buttons.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="css/forms.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="css/l10n.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="css/login.min.css" type="text/css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="index.html">Jack Parrish&#039;s Digital Resume and Portfolio</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://jackmparrish.com/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value size="20" autocapitalize="off" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value size="20" autocomplete="current-password" spellcheck="false">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="https://jackmparrish.com/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a href="wp-login_1.php">Lost your password?</a>			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="index.html">&larr; Go to Jack Parrish&#039;s Digital Resume and Portfolio</a>		</p>
+			</div>
+			<script type="text/javascript" defer="defer" src="js/jquery.min.js" id="jquery-core-js"></script>
+<script type="text/javascript" defer="defer" src="js/jquery-migrate.min.js" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/jackmparrish.com\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" defer="defer" src="js/zxcvbn-async.min.js" id="zxcvbn-async-js"></script>
+<script type="text/javascript" defer="defer" src="js/wp-polyfill-inert.min.js" id="wp-polyfill-inert-js"></script>
+<script type="text/javascript" defer="defer" src="js/regenerator-runtime.min.js" id="regenerator-runtime-js"></script>
+<script type="text/javascript" defer="defer" src="js/wp-polyfill.min.js" id="wp-polyfill-js"></script>
+<script type="text/javascript" defer="defer" src="js/hooks.min.js" id="wp-hooks-js"></script>
+<script type="text/javascript" defer="defer" src="js/i18n.min.js" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
+</script>
+<script type="text/javascript" defer="defer" src="js/password-strength-meter.min.js" id="password-strength-meter-js"></script>
+<script type="text/javascript" defer="defer" src="js/underscore.min.js" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" defer="defer" src="js/wp-util.min.js" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"8171e54e77"};
+/* ]]> */
+</script>
+<script type="text/javascript" defer="defer" src="js/user-profile.min.js" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
---
+++ wp-login_1.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Lost Password &lsaquo; Jack Parrish&#039;s Digital Resume and Portfolio &#8212; WordPress</title>
+	
+
+<style type="text/css">
+
+body.login {
+
+
+
+    }
+    
+body.login #login h1 a {
+
+
+
+
+
+}
+
+#loginform {
+
+
+
+
+
+}
+
+#login {
+
+}
+
+.login form .input, .login input[type="text"] {
+
+
+
+
+}
+
+.login label {
+
+}
+
+.wp-core-ui .button-primary {
+
+
+
+
+}
+
+.wp-core-ui .button-primary.focus, .wp-core-ui .button-primary.hover, .wp-core-ui .button-primary:focus, .wp-core-ui .button-primary:hover {
+
+
+}
+
+.login #backtoblog a, .login #nav a {
+
+}
+
+.login #backtoblog a:hover, .login #nav a:hover, .login h1 a:hover {
+
+}
+
+
+
+</style>
+
+<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link href="https://fonts.gstatic.com" crossorigin="anonymous" rel="preconnect">
+<link href="https://ajax.googleapis.com" rel="preconnect">
+<link href="https://fonts.googleapis.com" rel="preconnect">
+<link rel="stylesheet" id="dashicons-css" href="css/dashicons.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="css/buttons.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="css/forms.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="css/l10n.min.css" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="css/login.min.css" type="text/css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-lostpassword wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="index.html">Jack Parrish&#039;s Digital Resume and Portfolio</a></h1>
+	<p class="message">Please enter your username or email address. You will receive an email message with instructions on how to reset your password.</p>
+
+		<form name="lostpasswordform" id="lostpasswordform" action="https://jackmparrish.com/wp-login.php?action=lostpassword" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="user_login" id="user_login" class="input" value size="20" autocapitalize="off" autocomplete="username">
+			</p>
+						<input type="hidden" name="redirect_to" value>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Get New Password">
+			</p>
+		</form>
+
+		<p id="nav">
+			<a href="wp-login.php">Log in</a>
+					</p>
+				<p id="backtoblog">
+			<a href="index.html">&larr; Go to Jack Parrish&#039;s Digital Resume and Portfolio</a>		</p>
+			</div>
+					<script type="text/javascript">
+		try{document.getElementById('user_login').focus();}catch(e){}
+		if(typeof wpOnload==='function')wpOnload();
+		</script>
+			<div class="clear"></div>
+	</body>
+	</html>
+	
